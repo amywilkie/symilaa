d30b9448e94454fb4cab1b89411fc8b8ee1ea26d
user <skyhelpcentre@gmail.com>
2012-11-09 17:09:54 +0000

    CG|JE: Added more tests for known browser quirks
    Specifically IE9 with rounded corners looking pixelated and fields with a active state kept

--- test/symilaa/test_screenshot.rb
+++ test/symilaa/test_screenshot.rb
@@ -5,49 +5,49 @@ require_relative '../../lib/symilaa/screenshot'
 module Symilaa
   class TestScreenshot < Minitest::Unit::TestCase
     def test_equality_when_images_altered
-      one = Screenshot.new fixture 'bear_grylls.png'
-      two = Screenshot.new fixture 'bear_grylls_different.png'
+      one = Screenshot.new grylls
+      two = Screenshot.new grylls_different
 
       refute one == two
     end
 
     def test_equality_when_images_similar
-      one = Screenshot.new fixture 'bear_grylls.png'
-      two = Screenshot.new fixture 'bear_grylls_slightly_different.png'
+      one = Screenshot.new grylls
+      two = Screenshot.new grylls_slightly_different
 
       refute one == two
     end
 
     def test_equality_when_identical
-      one = Screenshot.new fixture 'bear_grylls.png'
-      two = Screenshot.new fixture 'bear_grylls.png'
+      one = Screenshot.new grylls
+      two = Screenshot.new grylls
 
       assert one == two
     end
 
     def test_similar_when_identical
-      one = Screenshot.new fixture 'bear_grylls.png'
-      two = Screenshot.new fixture 'bear_grylls.png'
+      one = Screenshot.new grylls
+      two = Screenshot.new grylls
 
       assert one.similar_to? two
     end
     
     def test_similar_when_similar
-      one = Screenshot.new fixture 'bear_grylls.png'
-      two = Screenshot.new fixture 'bear_grylls_slightly_different.png'
+      one = Screenshot.new grylls
+      two = Screenshot.new grylls_slightly_different
 
       assert one.similar_to? two
     end
    
     def test_different_sized_images_are_different
-      one = Screenshot.new fixture 'bear_grylls.png'
-      two = Screenshot.new fixture 'farewells.jpg'
+      one = Screenshot.new grylls
+      two = Screenshot.new the_queen
 
       refute one.similar_to? two
       refute one == two
     end
 
-    def test_font_size_difference
+    def test_css_font_size_change
       ref  = Screenshot.new fixture 'diagnostics/reference/font_size.png'
       diff = Screenshot.new fixture 'diagnostics/generated/font_size.png'
 
@@ -55,7 +55,7 @@ module Symilaa
       refute ref.similar_to? diff
     end
 
-    def test_gradient_difference
+    def test_css_change_of_gradient
       ref  = Screenshot.new fixture 'diagnostics/reference/gradient.png'
       diff = Screenshot.new fixture 'diagnostics/generated/gradient.png'
 
@@ -71,10 +71,42 @@ module Symilaa
       assert ref.similar_to?(diff), "Internet Explorer's rendering of a lightbox is similar enough each time"
     end
 
+    def test_rounded_corners_in_ie9
+      ref  = Screenshot.new fixture 'diagnostics/reference/rounded_corner_ie9.png'
+      diff = Screenshot.new fixture 'diagnostics/generated/rounded_corner_ie9.png'
+
+      refute ref == diff
+      assert ref.similar_to?(diff)
+    end
+
+    def test_active_input_state_in_ie9
+      ref  = Screenshot.new fixture 'diagnostics/reference/active_input_ie9.png'
+      diff = Screenshot.new fixture 'diagnostics/generated/active_input_ie9.png'
+
+      refute ref == diff
+      assert ref.similar_to?(diff)
+    end
+
     private
 
     def fixture name
       File.expand_path "../fixtures/#{name}", File.dirname(__FILE__)
+    end
+    
+    def grylls
+      fixture 'bear_grylls.png'
+    end
+
+    def grylls_different
+      fixture 'bear_grylls_different.png'
+    end
+
+    def grylls_slightly_different
+      fixture 'bear_grylls_slightly_different.png'
+    end
+
+    def the_queen
+      fixture 'farewells.jpg'
     end
   end
 end
