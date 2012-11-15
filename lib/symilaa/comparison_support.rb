869ff554694963ff4d3e1246391f723399b7d6d7
user <skyhelpcentre@gmail.com>
2012-11-15 15:48:06 +0000

    JE|NP: MASSIVE yep, MASSIVE refactor.  Boom.

--- lib/symilaa/comparison_support.rb
+++ lib/symilaa/comparison_support.rb
@@ -1,5 +1,13 @@
 module Symilaa
   module ComparisonSupport
+    def base_dir= dir
+      @base_dir = dir
+    end
+
+    def base_dir
+      @base_dir || 'artifacts'
+    end
+
     def add_text_to_image text, image
       txt = Magick::Draw.new
       image.annotate(txt, 0, 0, 0, 50, text) {
@@ -18,8 +26,7 @@ module Symilaa
       gif.write(output_file)
     end
 
-    def get_unique_file_name path, extension, inc
-      inc = 0 unless inc
+    def get_unique_file_name path, extension, inc=0
       inc = inc.to_int+1
 
       new_path = "#{path}#{inc}#{extension}"
@@ -31,42 +38,97 @@ module Symilaa
       end
     end
 
-    def check_screen_against_reference_shot sub_directory
-      sub_directory = "#{sub_directory}#{@gherkin_statement.parameterize.underscore}/"
+    def create_directories directories
+      directories.each do |dir|
+        path = File.join base_dir, dir
+        FileUtils.rm_rf(path) if File.exists? path
+        FileUtils.mkdir_p path
+      end
+    end
 
-      FileUtils.mkdir_p("#{SCREENSHOTS_GENERATED_THIS_RUN_DIR}/#{sub_directory}")
+    def browser_info
+      @browser_info ||= page.driver.browser.capabilities
+    end
 
-      browser_info = page.driver.browser.capabilities
+    def browser_name
+      browser_info.browser_name
+    end
 
-      version_if_required = "_#{browser_info.version.gsub('.', '-')}" unless browser_info.browser_name == 'firefox' and browser_info.version != '3.5' or browser_info.browser_name == 'chrome'
+    def browser_version
+      browser_info.version
+    end
 
-      title = find('title').text
-      file_name = "#{browser_info.browser_name.capitalize.parameterize}#{version_if_required}_#{browser_info.platform.capitalize}_#{title.parameterize.underscore}"
-      test_file = get_unique_file_name "#{SCREENSHOTS_GENERATED_THIS_RUN_DIR}/#{sub_directory}#{file_name}", '.png', nil
+    def title
+      find('title').text.parameterize.underscore
+    end
 
-      page.driver.browser.save_screenshot test_file
-
-      screenshot_name = File.basename test_file
-      reference_file = "#{REFERENCE_SCREENSHOTS}/#{sub_directory}#{screenshot_name}"
-
+    def same? reference_file, test_file
       reference_screenshot = Image.new reference_file
       generated_screenshot = Image.new test_file
 
-      same = if dodgy_browser?
-               reference_screenshot.similar_to? generated_screenshot
-             else
-               reference_screenshot == generated_screenshot
-             end
-
-      unless same
-        FileUtils.mkdir_p("#{DIFFERENCES_IN_SCREENSHOTS_THIS_RUN_DIR}/#{sub_directory}")
-        produce_gif_showing_the_difference_between(reference_file, test_file, "#{DIFFERENCES_IN_SCREENSHOTS_THIS_RUN_DIR}/#{sub_directory}#{screenshot_name}_difference.gif")
-        fail "Some of the screenshots did not match"
+      if dodgy_browser?
+        reference_screenshot.similar_to? generated_screenshot
+      else
+        reference_screenshot == generated_screenshot
       end
     end
 
     def dodgy_browser?
       %w[chrome ie7 ie8 ie9].include? ENV['REQUIRED_BROWSER']
+    end
+
+    def chrome_or_firefox_3_5
+      browser_name == 'chrome' or (browser_name == 'firefox' and browser_version == '3.5')
+    end
+
+    def version_if_required
+      "_#{browser_info.version.gsub('.', '-')}" if chrome_or_firefox_3_5? 
+    end
+
+    def reference_screenshots
+      File.join base_dir, 'reference_screenshots'
+    end
+
+    def screenshots_generated_this_run_dir
+      File.join base_dir, 'screenshots_generated_this_run'
+    end
+
+    def scenario_name
+      @gherkin_statement.parameterize.underscore if @gherkin_statement
+    end
+
+    def create_screenshot_directory sub_directory, scenario_name
+      FileUtils.mkdir_p(File.join screenshots_generated_this_run_dir, sub_directory, scenario_name)
+    end
+
+    def create_screenshot_filename sub_directory
+      file_name = "#{browser_info.browser_name.capitalize.parameterize}#{version_if_required}_#{browser_info.platform.capitalize}_#{title}"
+      get_unique_file_name "#{screenshots_generated_this_run_dir}/#{sub_directory}#{file_name}", '.png'
+    end
+
+    def save_screenshot screenshot_path
+      page.driver.browser.save_screenshot screenshot_path
+    end
+
+    def check_screen_against_reference_shot target_sub_directory
+      create_directories %w[reference_screenshots screenshots_generated_this_run differences_in_screenshots_this_run]
+      create_screenshot_directory target_sub_directory
+
+      generated_screenshot_path = create_screenshot_filename target_sub_directory
+      screenshot_name           = File.basename generated_screenshot_path
+      reference_path            = File.join reference_screenshots, target_sub_directory, screenshot_name
+      difference_file           = File.join path, "#{screenshot_name}_difference.gif"
+
+      save_screenshot generated_screenshot_path
+
+      unless same? reference_path, generated_screenshot_path
+        path = File.join differences_in_screenshots_this_run_dir, target_sub_directory
+        FileUtils.mkdir_p path
+
+        produce_gif_showing_the_difference_between reference_path, generated_screenshot_path, difference_file
+
+        fail "Some of the screenshots did not match"
+      end
     end
   end
 end
