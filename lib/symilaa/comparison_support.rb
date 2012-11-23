c8b16038610d3e913062032f045d7f6e54fab4f4
user <skyhelpcentre@gmail.com>
2012-11-23 11:11:09 +0000

    GR|MB: Enabled unique filename to be optional

--- lib/symilaa/comparison_support.rb
+++ lib/symilaa/comparison_support.rb
@@ -38,6 +38,11 @@ module Symilaa
       end
     end
 
+    def get_file_name path, extension
+      "#{path}#{extension}"
+    end
+
+
     def create_directories directories
       directories.each do |dir|
         FileUtils.mkdir_p File.join(base_dir, dir)
@@ -103,9 +108,13 @@ module Symilaa
       FileUtils.mkdir_p(File.join screenshots_generated_this_run_dir, sub_directory, scenario_name)
     end
 
-    def create_screenshot_filename sub_directory
+    def create_screenshot_filename sub_directory, unique_filename_required
       file_name = "#{browser_info.browser_name.capitalize.parameterize}#{version_if_required}_#{browser_info.platform.capitalize}_#{title}"
+      if unique_filename_required
       get_unique_file_name "#{screenshots_generated_this_run_dir}/#{sub_directory}#{scenario_name}/#{file_name}", '.png'
+      else
+        get_file_name "#{screenshots_generated_this_run_dir}/#{sub_directory}#{scenario_name}/#{file_name}", '.png'
+    end
     end
 
     def save_screenshot screenshot_path
@@ -116,11 +125,11 @@ module Symilaa
       "#{generated_screenshot_path}_difference.gif".gsub('screenshots_generated_this_run', 'differences_in_screenshots_this_run')
     end
 
-    def check_screen_against_reference_shot target_sub_directory
+    def check_screen_against_reference_shot target_sub_directory, unique_filename_required = true
       create_directories %w[screenshots_generated_this_run differences_in_screenshots_this_run]
       create_screenshot_directory target_sub_directory
 
-      generated_screenshot_path = create_screenshot_filename target_sub_directory
+      generated_screenshot_path = create_screenshot_filename target_sub_directory, unique_filename_required
       screenshot_name           = File.basename generated_screenshot_path
       reference_path            = File.join reference_screenshots, target_sub_directory, scenario_name, screenshot_name
       difference_file           = difference_file_path generated_screenshot_path
